182eb559dd02b029b4c846748a240a7587701b22
gardenee <snnjjang@naver.com>
2024-04-01 20:35:14 +0900

    ➕ svgr library 추가 - dev dependency - config 파일들 작성

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [],
+  plugins: [svgr()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'), // 프로젝트 루트의 src 디렉토리를 가리키는 별칭 설정
