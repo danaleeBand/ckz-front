65f933ac0bb57c4fcadb682a0847dab20a8af02e
gardenee <snnjjang@naver.com>
2024-04-01 22:14:10 +0900

    💄 tailwind custom theme 수정/추가 - confirm/info/warning/error color - borderWidth, fontSize extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,33 +14,49 @@ export default {
       'primary-lighter': '#988dc4',
       'primary-dark': '#534b7d',
       'primary-darker': '#453e75',
+
       secondary: '#0080c9',
       'secondary-light': '#4da8e0',
       'secondary-lighter': '#6bb8ea',
       'secondary-dark': '#006dbf',
       'secondary-darker': '#00528a',
-      'accent-a': '#f26c90',
-      'accent-b': '#ff9174',
-      'accent-c': '#ffc361',
-      'accent-d': '#f9f871',
-      'accent-e': '#b75ca5',
-      'accent-f': '#009ca5',
+
+      confirm: '#4caf50',
+      info: '#2196f3',
+      warning: '#ffd56b',
+      error: '#d9372b',
+
+      'accent-blossom': '#f26c90',
+      'accent-salmon': '#ff9174',
+      'accent-sunset': '#ffc361',
+      'accent-lemon': '#f9f871',
+      'accent-lavender': '#b75ca5',
+      'accent-sea': '#009ca5',
 
       // text & border color
       'text-primary': '#323232',
       'text-primary-light': '#595959',
       'text-primary-lighter': '#adacac',
-      'text-primary-inverse': '#f0eded',
+      'text-primary-inverse': '#f7f5f5',
 
       // background color
-      'bg-primary': '#fffbf0',
+      'bg-primary': '#faf9fa',
+      'bg-primary-dark': '#f2f2f2',
+      'bg-primary-darker': '#e8e6e8',
       // faf9fa f6f9fc fdf7f8 fffbf0 fef9fa faf6f8 fffbf0 fffafb
     },
     fontFamily: {
       sans: ['Noto Sans KR', 'sans-serif'],
       serif: ['Noto Sans KR', 'sans-serif'],
     },
-    extend: {},
+    extend: {
+      borderWidth: {
+        '1': '0.063rem', // 1px
+      },
+      fontSize: {
+        basic: '0.813rem', // 13px
+      },
+    },
   },
   plugins: [],
   darkMode: false,
