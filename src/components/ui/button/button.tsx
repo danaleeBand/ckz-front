f38c01225dc8900d798b9cd58caa55db8a1cd753
gardenee <snnjjang@naver.com>
2024-04-18 17:07:33 +0900

    💄 다크모드 색상 적용

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -24,22 +24,33 @@ export const Button = ({
   disabled,
 }: ButtonProps) => {
   const containedStyle = `
-    text-text-inverse border-none shadow-md
+    text-white border-none shadow-md
     ${
       color === 'primary'
-        ? 'bg-primary hover:bg-primary-dark active:bg-primary-darker'
-        : 'bg-secondary hover:bg-secondary-dark active:bg-secondary-darker'
+        ? `bg-primary hover:bg-primary-dark active:bg-primary-darker
+           dark:bg-dark-primary dark:hover:bg-dark-primary-dark dark:active:bg-dark-primary-darker`
+        : `bg-secondary hover:bg-secondary-dark active:bg-secondary-darker
+           dark:bg-dark-secondary dark:hover:bg-dark-secondary-dark dark:active:bg-dark-secondary-darker`
     }
   `;
 
   const outlinedStyle = `
-    bg-bg-basic hover:bg-bg-dark active:bg-bg-darker
+    bg-bg-basic active:bg-bg-dark
+    dark:bg-dark-bg-elevated dark:hover:bg-dark-bg-basic dark:active:bg-dark-bg-basic
     ${
       color === 'primary'
-        ? `text-text-primary border-primary hover:border-primary-dark hover:text-text-primary-dark 
-           active:border-primary-dark active:text-text-primary-dark`
-        : `text-text-secondary border-secondary hover:border-secondary-dark 
-           hover:text-text-secondary-dark active:border-secondary-dark active:text-secondary-dark`
+        ? `text-text-primary border-border-primary 
+           hover:text-text-primary-dark hover:border-border-primary-dark
+           active:text-text-primary-dark active:border-primary-dark
+           dark:text-dark-text-primary dark:border-dark-border-primary
+           dark:hover:text-dark-text-primary-light dark:hover:border-dark-border-primary-light
+           dark:active:text-dark-text-primary-light dark:active:border-dark-border-primary-light`
+        : `text-text-secondary border-border-secondary 
+           hover:border-border-secondary-dark hover:text-text-secondary-dark
+           active:border-border-secondary-dark active:text-text-secondary-dark
+           dark:text-dark-text-secondary dark:border-dark-border-secondary
+           dark:hover:border-dark-border-secondary-light dark:hover:text-dark-text-secondary-light
+           dark:active:border-dark-border-secondary-light dark:active:text-dark-text-secondary-light`
     }
   `;
 
