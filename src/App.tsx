941b927b72f92546c5bfc83bf3f5a097c3780534
gardenee <snnjjang@naver.com>
2024-04-21 18:11:20 +0900

    🐛 App.tsx modal set-element

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import Modal from 'react-modal';
 import './App.css';
 import { LoginPage, SignInPage } from '@/pages';
 
 function App() {
+  Modal.setAppElement('#root');
+
   return (
     <Routes>
       <Route
@@ -14,6 +17,7 @@ function App() {
           </GoogleOAuthProvider>
         }
       />
+      <Route path='/login' element={<></>} />
       <Route path='/join' element={<SignInPage />} />
     </Routes>
   );
