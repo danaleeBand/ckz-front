4fc7a234cfd2f527493e85222d8b0f553da69bd6
gardenee <snnjjang@naver.com>
2024-04-26 23:24:45 +0900

    ✨ useAxios hook에 interceptor 추가 - axios 호출시 baseURL 설정 - request header에 'Content-Type', 'access-token' 추가 - response interceptor 뼈대 작성

--- src/hooks/use-axios.ts
+++ src/hooks/use-axios.ts
@@ -1,15 +1,49 @@
 import { useEffect, useState } from 'react';
 import axios, { AxiosError, AxiosResponse, AxiosRequestConfig } from 'axios';
-
-axios.defaults.baseURL = ''; // TODO: BASE API URL
+import { useAuthStore } from '@/stores';
 
 export const useAxios = (axiosParams: AxiosRequestConfig) => {
   const [response, setResponse] = useState<AxiosResponse>();
   const [error, setError] = useState<AxiosError>();
   const [loading, setLoading] = useState<boolean>(true);
 
+  const { accessToken } = useAuthStore();
+
+  const instance = axios.create({
+    baseURL: import.meta.env.BASE_URL,
+    timeout: 1000,
+  });
+
+  instance.interceptors.request.use(
+    config => {
+      config.headers['Content-Type'] = 'application/json';
+      config.headers['access-token'] = accessToken;
+
+      return config;
+    },
+    error => {
+      console.error('axios request error', error);
+      return Promise.reject(error);
+    },
+  );
+
+  instance.interceptors.response.use(
+    response => {
+      // TODO: 오류 페이지 interceptor 추가
+      return response;
+    },
+    async error => {
+      if (error.response?.status === 401) {
+        // TODO: 토큰 말료시 refreshToken으로 accessToken 갱신
+        console.log('interceptor 401 error', error);
+        return instance.request(error.config);
+      }
+      return Promise.reject(error);
+    },
+  );
+
   const fetchData = async (params: AxiosRequestConfig) => {
-    await axios
+    await instance
       .request(params)
       .then(res => {
         setResponse(res);
