db6c9842fef8e3af061e5a275e7ee2b24fe4a3f8
gardenee <snnjjang@naver.com>
2024-04-30 11:07:28 +0900

    ♻️ 라우팅 구조 개선, 인증 여부 따른 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import Modal from 'react-modal';
 import './App.css';
 import { Checklist, LoginPage, SignInPage } from '@/pages';
+import { Auth, ProtectedRoute } from '@/routes';
 
 function App() {
   Modal.setAppElement('#root');
 
   return (
     <Routes>
-      <Route
-        path='/'
-        element={
-          <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-            <LoginPage />
-          </GoogleOAuthProvider>
-        }
-      />
-      <Route path='/login' element={<></>} />
-      <Route path='/join' element={<SignInPage />} />
-      <Route path='/:checklist-id' element={<Checklist />} />
+      <Route path='/' element={<LoginPage />} />
+      <Route path='/login/:provider' element={<Auth />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path='/join' element={<SignInPage />} />
+        <Route path='/:checklist-id' element={<Checklist />} />
+      </Route>
     </Routes>
   );
 }
