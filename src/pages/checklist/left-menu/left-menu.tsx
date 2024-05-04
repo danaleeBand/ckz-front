c983875933a4f61dcd0bd2ef503dba46830d24f7
gardenee <snnjjang@naver.com>
2024-05-04 21:34:18 +0900

    💄 box-shadow-right 설정

--- src/pages/checklist/left-menu/left-menu.tsx
+++ src/pages/checklist/left-menu/left-menu.tsx
@@ -2,7 +2,7 @@ import { TreeMenu } from './tree';
 
 export const LeftMenu = () => {
   return (
-    <div>
+    <div className='box-shadow-right'>
       <TreeMenu />
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -210,6 +210,9 @@ export default {
         },
       },
     },
+    boxShadow: {
+      right: '10px 0 10px -10px rgba(0, 0, 0, 0.3)',
+    },
   },
   plugins: [],
   corePlugins: {
