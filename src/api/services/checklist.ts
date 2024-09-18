4084be8afc9742c5ec42a515a0d2fde335a81291
garden <garden22@lgcns.com>
2024-09-18 14:33:21 +0900

    👽️ folder, checklist api 추가

---
+++ src/api/services/checklist.ts
@@ -0,0 +1,35 @@
+import apiRequest from '../api';
+
+export type CreateChecklistResponseType = {
+  id: number;
+};
+
+export const postChecklist = async (
+  folderId: number,
+  checklistName: string,
+) => {
+  return apiRequest(`/folders/${folderId}/checklists`, {
+    method: 'POST',
+    data: { title: checklistName },
+  });
+};
+
+export const patchChecklist = async (
+  checklistId: number,
+  checklistName: string,
+  folderId: number,
+) => {
+  return apiRequest(`/folders/${folderId}/checklist/${checklistId}`, {
+    method: 'PATCH',
+    data: { title: checklistName },
+  });
+};
+
+export const deleteChecklist = async (
+  checklistId: number,
+  folderId: number,
+) => {
+  return apiRequest(`/folders/${folderId}/checklist/${checklistId}`, {
+    method: 'DELETE',
+  });
+};
---
+++ src/api/services/folder.ts
@@ -0,0 +1,23 @@
+import apiRequest from '../api';
+
+export type CreateFolderResponseType = {
+  id: number;
+};
+
+export const postFolder = async (workspaceId: number, folderName: string) => {
+  return apiRequest(`/workspaces/${workspaceId}/folders`, {
+    method: 'POST',
+    data: { name: folderName },
+  });
+};
+
+export const patchFolder = async (
+  workspaceId: number,
+  folderId: number,
+  folderName: string,
+) => {
+  return apiRequest(`/workspaces/${workspaceId}/folder/${folderId}`, {
+    method: 'PATCH',
+    data: { name: folderName },
+  });
+};
