d8ad2ceb175adffbeccbc01ae02caaca9ba79ab8
garden <garden22@lgcns.com>
2024-11-09 21:48:04 +0900

    ✨ accessToken 갱신 실패시 alert 후 홈으로 라우팅

--- src/api/axios-instance.ts
+++ src/api/axios-instance.ts
@@ -71,6 +71,8 @@ axiosInstance.interceptors.response.use(
         })
         .catch(err => {
           processFailedQueue(err);
+          alert('세션이 만료되었습니다. 다시 로그인해주세요.');
+          window.location.href = '/'; // TODO: history 사용하도록 수정
           return Promise.reject(err);
         })
         .finally(() => {
