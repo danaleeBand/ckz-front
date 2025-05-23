bede9e7358127f5ba2c153cc7a3161fc859fe8d9
garden <>
2025-05-23 18:27:46 +0900

    🔧 strict mode develop에서만 사용되도록

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
@@ -8,9 +9,13 @@ import { queryClient } from './api/index.ts';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
-      {/* <React.StrictMode> */}
-      <App />
-      {/* </React.StrictMode> */}
+      {import.meta.env.MODE === 'production' ? (
+        <App />
+      ) : (
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      )}
     </QueryClientProvider>
   </BrowserRouter>,
 );
