1145998845f7ad87b2006452f5dcfdf5f76f365e
garden <>
2025-05-23 20:00:59 +0900

    🔥 react strict mode 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
@@ -9,13 +8,7 @@ import { queryClient } from './api/index.ts';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
-      {import.meta.env.MODE === 'production' ? (
-        <App />
-      ) : (
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>
-      )}
+      <App />
     </QueryClientProvider>
   </BrowserRouter>,
 );
